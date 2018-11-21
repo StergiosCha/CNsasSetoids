7990c98db3af1f8ac793ce148b2df7e98f4ea4c4
StergiosCha <kafouroutsos@hotmail.com>
2018-11-21 23:49:35 +0100

    Update CNs_as_Setoids.v

--- CNs_as_Setoids.v
+++ CNs_as_Setoids.v
@@ -86,19 +86,24 @@ Theorem pickedupMasteredPHY:(three Book2) (AND(book)(picked_up john)(mastered jo
 
 Section BOOK.
 Variable BOOK1BOOK2: forall x:book, forall y:book, IC_Book1  x y-> IC_Book2 x y.
- Theorem pick1edupMasteredPHY:(three Book2) (AND(book)(picked_up john)(mastered john))-> (three PHY)(picked_up  john).
-   cbv. intros. destruct H. destruct H. destruct H. destruct H0. destruct H0. destruct H0. destruct H2. destruct H2. destruct H2. destruct H4. exists x. split. intuition. intuition.  exists x0. intuition. intuition. exists x1. split. intuition.      split. destruct IC_Book2 in BOOK1BOOK2. destruct IC_Book1 in BOOK1BOOK2. destruct _eq3. destruct a. destruct _eq4. destruct a.      destruct IC_Info in H4. destruct IC_Phy. destruct IC_Info in H7. destruct IC_Info in H8. destruct _eq5. destruct _eq4. destruct _eq6. destruct H9. destruct _eq3. destruct H13. destruct H11. unfold transitive in H18. unfold symmetric in H11. unfold reflexive in H10. unfold transitive in H17. unfold symmetric in H13. unfold reflexive in H12. unfold transitive in H14. unfold symmetric in H9. unfold reflexive in H6. destruct H16. unfold transitive in H19. unfold symmetric in H16. unfold reflexive in H15. firstorder.  Abort all. 
+Theorem pick1edupMasteredPHY:(three Book2) (AND(book)(picked_up john)(mastered john))-> (three PHY)(picked_up  john). 
+cbv. intros. destruct H. destruct H. destruct H. destruct H0. destruct H0. destruct H0. destruct H2. destruct H2. 
+destruct H2. destruct H4. exists x. split. intuition. intuition.  exists x0. intuition. intuition. exists x1. split. 
+intuition. split. destruct IC_Book2 in BOOK1BOOK2. destruct IC_Book1 in BOOK1BOOK2. destruct _eq3. destruct a. 
+destruct _eq4. destruct a. destruct IC_Info in H4. destruct IC_Phy. destruct IC_Info in H7. destruct IC_Info in H8. d
+estruct _eq5. destruct _eq4. destruct _eq6. destruct H9. destruct _eq3. destruct H13. destruct H11. unfold transitive in H18.
+unfold symmetric in H11. unfold reflexive in H10. unfold transitive in H17. unfold symmetric in H13. unfold reflexive in H12.
+unfold transitive in H14. unfold symmetric in H9. unfold reflexive in H6. destruct H16. unfold transitive in H19. 
+unfold symmetric in H16. unfold reflexive in H15. firstorder.  Abort all. 
 
-
- 
- Definition Three_dot2:=fun c: Book1.(B)->PHYINFO.(B)=>fun P:PHYINFO.(B)->Prop=>exists x y z: Book1.(B), not((PHY.(B2))(c(x))(c(y)))/\not((PHY.(B2))(c(y))(c(z))) /\not((PHY.(B2))(c(x))(c(z)))/\not((INFO.(B2))(c(x))(c(y)))/\not((INFO.(B2))(c(y))(c(z))) /\not((INFO.(B2))(c(x))(c(z)))/\P (c(x))/\ P(c(y))/\P(c(z)). (**arbitrary A, WHEN SUBTYPING WORKS C SHOULD BE REDUNDANT, CHECK IT**)
+Definition Three_dot2:=fun c: Book1.(B)->PHYINFO.(B)=>fun P:PHYINFO.(B)->Prop=>exists x y z: Book1.(B), not((PHY.(B2))(c(x))(c(y)))/\not((PHY.(B2))(c(y))(c(z))) /\not((PHY.(B2))(c(x))(c(z)))/\not((INFO.(B2))(c(x))(c(y)))/\not((INFO.(B2))(c(y))(c(z))) /\not((INFO.(B2))(c(x))(c(z)))/\P (c(x))/\ P(c(y))/\P(c(z)). (**arbitrary A, WHEN SUBTYPING WORKS C SHOULD BE REDUNDANT, CHECK IT**)
  
 Definition and:= fun A:DomCN=>  fun P: A->Prop=>fun Q: A->Prop=>fun x: A=>  P(x)/\Q(x).
 Axiom c1: Book1.(B)->PHYINFO.(B). Axiom c2: Book2.(B)->PHYINFO.(B).
- (**John picked up and mastered three books->John mastered three informational objects**)
+                  (**John picked up and mastered three books->John mastered three informational objects**)
 Theorem pickedmastered3m: (Three_dot2 c1)(and PHYINFO(picked_up john )(mastered john))-> exists x y z: Book1.(B),mastered (john)(c1 x)/\mastered (john)(c1 y)/\mastered (john)(c1 z)/\ not((INFO.(B2))(c1(x))(c1(y)))/\not((INFO.(B2))(c1(y))(c1(z)))/\not((INFO.(B2))(c1(x))(c1(z))). cbv. firstorder. Qed.
 
-                     (**John picked up and mastered three books->John picked_up three physical objects**)                                                                                           
+                   (**John picked up and mastered three books->John picked_up three physical objects**)                                                                                           
 Theorem pickedmastered3p: (Three_dot2 c1)(and PHYINFO(picked_up john )(mastered john))-> exists x y z: Book1.(B),picked_up (john)(c1 x)/\picked_up (john)(c1 y)/\picked_up (john)(c1 z)/\ not((PHY.(B2))(c1(x))(c1(y)))/\not((PHY.(B2))(c1(y))(c1(z)))/\not((PHY.(B2))(c1(x))(c1(z))). cbv. firstorder. Qed.
 
   
@@ -132,40 +137,32 @@ Definition IC_HBook2:=  mkEquiv heavybook  AIC_HBook2 EQ5.
 Definition HBook2:= mkCN heavybook IC_HBook2.
 
 Theorem HEAVYBOOK:( (some HBook2) ( mastered john))-> (some INFO)( mastered john). firstorder. Qed.
-
 Theorem HEAVYBOOK1:( (some HBook1) (picked_up john))-> (some PHY)( picked_up john). firstorder. Qed.
 Theorem HEAVYBOOK2:( (three HBook1) (picked_up john))-> (three PHY)( picked_up john). cbv.  firstorder. Qed.
-
 Theorem HEAVYBOOK3:( (three HBook2) ( mastered john))-> (three INFO)( mastered john). cbv. intros. firstorder. Qed.
 
 Parameter informative: Info->Prop.
 Axiom c3: HBook1.(B)->PHYINFO.(B). Axiom c4: HBook2.(B)->PHYINFO.(B).
-
-
 Record heavyinfobook :DomCN := mkheavyinfobook { l4 :>heavybook; _ : informative l4 }.
 
 Definition AIC_HIBook1 := fun x: heavyinfobook=>fun y:heavyinfobook=> IC_HBook1(x)(y).
 Theorem EQ6: reflexive  heavyinfobook  AIC_HIBook1/\  symmetric heavyinfobook AIC_HIBook1/\  transitive heavyinfobook AIC_HIBook1. cbv. destruct  IC_Phy. destruct _eq3. destruct H0. split. intro. unfold reflexive in H.  apply H. split. intro. unfold symmetric in H0. intuition. intro. unfold transitive in H1. intro. intro. intros. apply H1 with y. assumption. assumption. Qed.
-
 Definition IC_HIBook1:=  mkEquiv heavyinfobook  AIC_HIBook1 EQ6.
 Definition HIBook1:= mkCN heavyinfobook IC_HIBook1.
 
 Definition AIC_HIBook2 := fun x: heavyinfobook=>fun y:heavyinfobook=> IC_HBook2(x)(y).
 Theorem EQ7: reflexive  heavyinfobook  AIC_HIBook2/\  symmetric heavyinfobook AIC_HIBook2/\  transitive heavyinfobook AIC_HIBook2. cbv. destruct  IC_Info. destruct _eq3. destruct H0. split. intro. unfold reflexive in H.  apply H. split. intro. unfold symmetric in H0. intuition. intro. unfold transitive in H1. intro. intro. intros. apply H1 with y. assumption. assumption. Qed.
-
 Definition IC_HIBook2:=  mkEquiv heavyinfobook  AIC_HIBook2 EQ7.
 Definition HIBook2:= mkCN heavyinfobook IC_HIBook2.
 
 
 Theorem HEAVYIBOOK2:( (three HIBook1) (picked_up john))-> (three PHY)( picked_up john). cbv. firstorder. Qed.
-
 Theorem HEAVYIBOOK3:( (three HIBook2) ( mastered john))-> (three INFO)( mastered john). cbv. intros. firstorder.                                                 Qed.
 Theorem HEAVYIBOOK4:( (three HIBook1) (picked_up john))-> (three HBook1)( picked_up john). cbv.  firstorder. Qed.
 Theorem HEAVYIBOOK5:( (three HIBook2) ( mastered john))-> (three HBook2)( mastered john). cbv. intros. firstorder.                                                 Qed.
 Theorem HEAVYIBOOK6:( (three HBook1) (picked_up john))-> (three Book1)( picked_up john). cbv.  firstorder. Qed.
 Theorem HEAVYIBOOK7:( (three HBook2) ( mastered john))-> (three Book2)( mastered john). cbv. intros. firstorder.                                                 Qed.
 Axiom HB: HBook1.(B)->Book1.(B). 
-
 Theorem HEAVYIBOOK8: (three HBook1)((picked_up john ))->    three Book1 (Heavy).
 decompose record HBook1.  cbv. decompose record heavybook. intros.  firstorder. decompose record H0. decompose record x.
 decompose record x. exists l5. split. assumption.  decompose record x0. exists l6. split. assumption. decompose record x1. 
@@ -175,8 +172,5 @@ exists l7. split. assumption. firstorder.  Abort all. End BOOK.
 Definition Three_d:=fun A:CN=> fun c: A.(B)->PHYINFO.(B)=>fun P:PHYINFO.(B)->Prop=>exists x y z: A.(B), not((PHY.(B2))(c(x))(c(y)))/\not((PHY.(B2))(c(y))(c(z))) /\not((PHY.(B2))(c(x))(c(z)))/\not((INFO.(B2))(c(x))(c(y)))/\not((INFO.(B2))(c(y))(c(z))) /\not((INFO.(B2))(c(x))(c(z)))/\P (c(x))/\ P(c(y))/\P(c(z)).
 
 Theorem masteredheavy: (Three_d HBook1 bf)(mastered john)-> exists x y z: heavybook, not((PHY.(B2))((x))((y)))/\not((PHY.(B2))((y))((z))) /\not((PHY.(B2))((x))((z))). cbv. firstorder. Qed.
-
-
 Theorem masteredheavy1: (Three_d HBook1 bf)(mastered john)-> (three Book1) Heavy. cbv.  firstorder. exists x. split. 
-apply (x.(L)). 
-exists x0. split.  apply (x0.(L)).    exists x1. split. apply (x1.(L)). firstorder.  Qed.
+apply (x.(L)).  exists x0. split.  apply (x0.(L)).    exists x1. split. apply (x1.(L)). firstorder.  Qed.
